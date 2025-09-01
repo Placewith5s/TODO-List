d73ae9a37673ebcb8b5644f022ab681b7723ac71
Placewith5s <qsenkaisu@gmail.com>
2025-09-01 19:43:40 +0200

    Fixed remove button and warning
    Fixed remove button references and changed 'this.main' to '!this.main' to fix the warning

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
                 this.new_list_parent.className = "added-lists";
                 this.main.append(this.new_list_parent);
 
-                if (this.main || !this.title || !this.add_btn || !this.new_list_parent) {
+                if (!this.main || !this.title || !this.add_btn || !this.new_list_parent) {
                     console.warn("Missing items!");
                 } else {
                     console.log("Items found");
@@ -25,39 +25,40 @@
             search_func() {
                 try {
                     // create the new list
-                    this.new_list = document.createElement("li");
-                    this.new_list_parent.append(this.new_list);
+                    const new_list = document.createElement("li");
+                    this.new_list_parent.append(new_list);
 
                     // create the remove button
-                    this.remove_btn = document.createElement("button");
-                    this.remove_btn.textContent = "Remove";
-                    this.remove_btn.type = "button";
-                    this.remove_btn.id = "remove-btn";
-                    this.remove_btn.ariaLabel = this.remove_btn.textContent;
-                    this.main.append(this.remove_btn);
-                    this.remove_list_n_btn();
+                    const remove_btn = document.createElement("button");
+                    remove_btn.textContent = "Remove";
+                    remove_btn.type = "button";
+                    remove_btn.id = "remove-btn";
+                    remove_btn.ariaLabel = remove_btn.textContent;
+                    this.main.append(remove_btn);
+                    this.remove_list_n_btn(new_list, remove_btn);
 
                     // empty input value handler
                     if (this.title.value.trim() != "") { // make space count as empty
-                        this.new_list.textContent = this.title.value;
+                        new_list.textContent = this.title.value;
                     } else {
                         console.warn("Write a value before clicking on 'Add'!")
-                        this.new_list.remove();
-                        this.remove_btn.remove();
+                        new_list.remove();
+                        remove_btn.remove();
                     }
                 } catch (err) {
                     console.error("Cannot create elements:", err);
                 }
             }
 
-            remove_list_n_btn() {
+            remove_list_n_btn(new_list, remove_btn) {
                 this.remove_btn.addEventListener("click", () => {
-                    this.new_list.remove();
-                    this.remove_btn.remove();
+                    new_list.remove();
+                    remove_btn.remove();
                 });
             }
         }
         // call the class
         new TODO_List;
     });
+
 })();
